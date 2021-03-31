5c2f9c72591a579b628de9351fcb8c94fd368eca
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-03-31 09:59:11 -0300

    feat: add integration with google analytics

---
+++ pages/_document.js
@@ -0,0 +1,41 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BSDWW4LMHQ"
+          ></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7NMZB1N673');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
